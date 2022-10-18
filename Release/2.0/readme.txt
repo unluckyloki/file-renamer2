11927f21ae02ae651e8738db197b86cb5735e413
BuildTools <unconfigured@null.spigotmc.org>
2022-10-18 23:05:00 +0700

    First commit

---
+++ Release/2.0/readme.txt
@@ -0,0 +1,9 @@
+Requires .NET 6.0+
+
+You can install latest .NET here: https://dotnet.microsoft.com/en-us/download/dotnet
+
+---
+
+Требуется .NET версии 6.0 или выше
+
+Последнюю версию .NET можно установить по данной ссылке: https://dotnet.microsoft.com/en-us/download/dotnet
---
+++ Source/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "File renamer";
+
+            while (true)
+            {
+                Console.Clear();
+                string[] logo = {
+                "╔╗       ╔╗    ",
+                "║║       ║║    ",
+                "║║   ╔══╗║║╔╗╔╗",
+                "║║ ╔╗║╔╗║║╚╝╝╠╣",
+                "║╚═╝║║╚╝║║╔╗╗║║",
+                "╚═══╝╚══╝╚╝╚╝╚╝"
+            };
+                for (int i = 0; i < logo.Length; i++)
+                {
+                    Console.WriteLine(logo[i]);
+                }
+
+                try
+                {
+                    Console.Write("\nEnter directory path: ");
+                    string dirPath = Console.ReadLine();
+                    Console.Write("Enter start num: ");
+                    int startNum = Convert.ToInt32(Console.ReadLine());
+
+                    string[] filesList = Directory.GetFiles(dirPath);
+
+                    for (int i = 0; i < filesList.Length; i++)
+                    {
+                        int sInd = filesList[i].LastIndexOf('\\');
+                        int dInd = filesList[i].LastIndexOf('.');
+                        string fileFormat = filesList[i].Substring(dInd);
+                        int fileNum = i + startNum;
+                        File.Move(filesList[i], filesList[i].Remove(sInd + 1) + fileNum + fileFormat);
+                    }
+
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("\nSuccessful operation\n");
+                    Console.ResetColor();
+                }
+                catch (Exception e)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("\nSomething went wrong :D\n");
+                    Console.WriteLine(e + "\n");
+                    Console.ResetColor();
+                }
+
+                Console.WriteLine("[1] Rename another files");
+                var key = Console.ReadKey(true).Key;
+                do{
+                    if (key == ConsoleKey.D1) break;
+                    key = Console.ReadKey(true).Key;
+                } while(key != ConsoleKey.D1 || key != ConsoleKey.D2);
+            }
+        }
+    }
+}
